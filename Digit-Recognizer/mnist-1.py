ec3d9c594b7b84d201e2cebfbd36b69f158cbb90
Unknown <regwanul.i@gmail.com>
2017-10-26 21:31:57 -0400

    Tweaking model for better Kaggle result

--- Digit-Recognizer/mnist-1.py
+++ Digit-Recognizer/mnist-1.py
@@ -15,10 +15,13 @@ X_test = (pd.read_csv('input/test.csv').values).astype('float32')
 y_train = to_categorical(labels) 
 
 # Feature Scaling and Mean Normalization
-mean = X_train.mean().astype(np.float32)
-std = X_train.std().astype(np.float32)
-X_train = (X_train - mean)/std
-X_test = (X_test - mean)/std
+scale = np.max(X_train)
+X_train /= scale
+X_test /= scale
+
+mean = np.std(X_train)
+X_train -= mean
+X_test -= mean
 
 input_dim = X_train.shape[1]
 num_classes = y_train.shape[1]
@@ -26,7 +29,9 @@ num_classes = y_train.shape[1]
 # 3-layer network
 model = Sequential()
 model.add(Dense(784, activation='sigmoid', input_dim=input_dim))
+model.add(Dropout(0.2))
 model.add(Dense(784, activation='sigmoid'))
+model.add(Dropout(0.2))
 model.add(Dense(num_classes, activation='sigmoid'))
 
 model.summary()
@@ -34,10 +39,10 @@ model.summary()
 model.compile(loss='mean_squared_logarithmic_error', optimizer='adam', metrics=['accuracy'])
 
 print("Training...")
-model.fit(X_train, y_train, epochs=10, batch_size=64, validation_split=0.3, verbose=2)
+model.fit(X_train, y_train, epochs=20, batch_size=64, validation_split=0.3, verbose=1)
 
 print("Generating test predictions...")
-preds = model.predict_classes(X_test, verbose=2)
+preds = model.predict_classes(X_test, verbose=1)
 
 submission=pd.DataFrame({"ImageId": list(range(1,len(preds)+1)), "Label": preds})
 submission.to_csv("mnist-predictions.csv", index=False, header=True)
